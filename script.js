3b0c84d72b60899c0dba5e051eabd1ddfecef591
Roman Pchelkin <epos.91@mail.ru>
2022-06-23 13:33:42 +0300

    upd 1

---
+++ script.js
@@ -0,0 +1,18 @@
+// const logBtn = document.querySelector('.login-btn');
+
+// logBtn.addEventListener('click', (e) => {
+// 	e.preventDefault();
+	
+// 	const userName = document.querySelector('.username');
+// 	const userPass = document.querySelector('.userpass');
+
+// 	if (userName.value == 'admin' && userPass.value == 'admin') {
+// 		const wrappers = document.querySelectorAll('.wrapper');
+// 		const loginModal = document.querySelector('.login');
+// 		wrappers.forEach(e => {
+// 			e.classList.remove('wrapper-off');
+// 		});
+// 		loginModal.classList.add('wrapper-off');
+// 	}
+
+// });
